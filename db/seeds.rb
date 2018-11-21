24f68e924042f245eaace700b415bba5f79b58a7
Caroline Maze <caroline.maze@hotmail.fr>
2018-11-21 19:17:36 +0100

    exo resto

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+  Restaurant.create(name: 'SushiShop', address: 'Paris 16', category: 'japanese', phone_number: '01 46 75 29 13')
+  Restaurant.create(name: 'Fuxia', address: 'Paris 15', category: 'italian', phone_number: '01 46 75 29 12')
+  Restaurant.create(name: 'Frichti', address: 'Paris 12', category: 'french', phone_number: '01 47 75 29 13')
+  Restaurant.create(name: 'Mcdo', address: 'Paris 18', category: 'belgian', phone_number: '01 46 75 29 15')
+  Restaurant.create(name: 'PNY', address: 'Paris 11', category: 'french', phone_number: '01 46 76 29 13')
+
